3aefc08835934cb9578b9b492142db793e784d07
Wagner Paulino <wagnersantos9778@gmail.com>
2020-04-11 23:46:41 -0300

    commit inicial

---
+++ src/main/kotlin/com/firstProjectoKotlinSpringBoot/FirstProjectoKotlinSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.firstProjectoKotlinSpringBoot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstProjectoKotlinSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstProjectoKotlinSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/firstProjectoKotlinSpringBoot/FirstProjectoKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.firstProjectoKotlinSpringBoot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstProjectoKotlinSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
